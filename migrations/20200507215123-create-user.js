a6fe9dddb057e1db24d41b61a8951a3df96ac8ef
ahmedhijazi94 <ahmedhijazi94@gmail.com>
2020-05-07 18:59:15 -0300

    adicionando model usuario

---
+++ migrations/20200507215123-create-user.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        trim: true
+      },
+      login: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        trim: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        trim: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      trim: true
+    },
+    login:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      trim: true
+    },
+    password:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      trim: true
+    }
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
