dd724a6b499a3cfcb6cd31ee1ffa3a52bf6f6a86
ahmedhijazi94 <ahmedhijazi94@gmail.com>
2020-05-07 16:59:09 -0300

    configurando dev

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) =>{
+    res.send('ok');
+});
+
+
+app.listen('3001', () =>{
+    console.log("server running on 3001");
+})
+
+
