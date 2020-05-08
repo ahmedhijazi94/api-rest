4e97e25daec66cd5ad2d021c9a93305d93e0679e
ahmedhijazi94 <ahmedhijazi94@gmail.com>
2020-05-08 14:39:15 -0300

    encriptando a senha do usuario antes do cadastro

--- controllers/users/userController.js
+++ controllers/users/userController.js
@@ -1,16 +1,22 @@
 const User = require('../../models').User;
-
+const bcrypt = require('bcrypt');
 
 module.exports = {
     async register(req, res){
-       const {login, email} = req.body;
+       const {login, email, password} = req.body;
        try {
+           //checa se login existe.
            if(await User.findOne({where: {login : login}})){
                return res.status(400).send('User already exists.');
            }
+           //checa se email existe
            if(await User.findOne({where: {email: email}})){
                 return res.status(400).send('E-mail already exists.');
            }
+           //encriptando a senha.
+           const hash = await bcrypt.hash(password, 10);
+           req.body.password = hash;
+           //criar user
            await User.create(req.body).then(user => res.send(user));
            return;
            
