9de4a20763208717744195855605d6715edd6a7c
ahmedhijazi94 <ahmedhijazi94@gmail.com>
2020-05-14 12:53:24 -0300

    adicionando cors

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+var cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
 
 
 app.use('/api', require('./src/routes.js'));
